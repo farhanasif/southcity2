3707a221459c124de94a01caad15f5a2e7aa556a
Admin <admin@Admins-MacBook-Pro.local>
2020-10-17 20:02:04 +0600

    basic config

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import React from 'react';
-import {
-  Text,
-} from 'react-native';
-
-
-const App = () => {
+function HomeScreen() {
   return (
-    <>
-      <Text>Hello World</Text>
-    </>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
   );
-};
+}
 
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
